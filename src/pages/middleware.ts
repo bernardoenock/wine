8bafd9bfbc1a95867d7c558285af0673a1f9e9ee
Bernardo Enock <bernardoenockgm@gmail.com>
2022-09-05 19:05:30 -0300

    feat: deploy

--- src/pages/middleware.ts
+++
@@ -1,8 +0,0 @@
-// import type { NextFetchEvent, NextRequest } from "next/server";
-
-// import { NextResponse } from "next/server";
-
-// export function middleware(req: NextRequest, ev: NextFetchEvent) {
-//   return NextResponse.redirect("http://localhost:3000/");
-// }
-export {};
